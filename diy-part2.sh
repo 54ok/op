5f2a6646658eaa6a0c3d8b0272d53a9a597bff4c
54ok <48821295+54ok@users.noreply.github.com>
2022-01-01 22:04:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.54/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.154/g' package/base-files/files/bin/config_generate
+
+# 2.清除登录密码
+sed -i ' s/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g ' openwrt/package/lean/default-settings/files/zzz-default-settings
